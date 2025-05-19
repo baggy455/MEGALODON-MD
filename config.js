1e2dfadd82ee5b075a86886597478337b9d5a028
baggy455 <andyrussell171@gmail.com>
2025-05-19 17:35:11 +0100

    .

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~XFoDkTqZ#dQP9xN1GLcUHo17oM5YrPqKC0pfiWuphwxicbN14QDQ",
+SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~CUoiTLpS#Prq0R3Qoeb4pt55PPOjoYbj2JGrD6TuzTBk8zcs5wbM",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
